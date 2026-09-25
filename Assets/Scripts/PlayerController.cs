39c4ac3423901b7eaa47eb6c073cfe9d05891fc6
XendeRrRrR <sanderbratteli@gmail.com>
2026-09-25 11:09:12 +0200

    DLC

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameObject.name = "Markus Lange";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            print("hi");
+        }
     }
 }
