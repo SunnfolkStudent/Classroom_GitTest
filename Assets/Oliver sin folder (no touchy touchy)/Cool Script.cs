34b6b43b96d68f29373fb07776ac4794f69288a4
Oliver <oliverselle68@gmail.com>
2026-09-25 11:03:31 +0200

    Oliver's very cool update
    this will make game very good and 0 bugs :D

---
+++ Assets/Oliver sin folder (no touchy touchy)/Cool Script.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class CoolScript : MonoBehaviour
+{
+    public string Game;
+
+    private void Start()
+    {
+        Game = "very good and working, 0 bugs";
+    }
+}
